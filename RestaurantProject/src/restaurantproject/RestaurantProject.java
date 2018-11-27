12864fe62dffa14cd608fea4edbfe387b0ceef38
Jalyway <39788981+Jalyway@users.noreply.github.com>
2018-11-27 20:57:15 +0800

    Add files via upload

---
+++ RestaurantProject/src/restaurantproject/RestaurantProject.java
@@ -0,0 +1,31 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantproject;
+
+import java.awt.Frame;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Wang
+ */
+public class RestaurantProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+    }
+    
+}
